b0cac219db970e77d37438f467488534ebc0e09e
James Fargher <proglottis@gmail.com>
2016-04-07 12:56:51 +1200

    Initial client js

--- js/client.js
+++ js/client.js
@@ -1,1 +1,34 @@
 "use strict";
+
+$(function() {
+  var $log = $("#log");
+
+  function appendLog(msg) {
+    $log.append($("<div>").text(msg));
+  }
+
+  var conn;
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    conn = new WebSocket($('body').data('url'));
+
+
+    conn.onopen = function(event) {
+      appendLog("Connection opened");
+      conn.send(JSON.stringify({Type: 'join', Code: $('input[name=code]').val()}))
+    };
+
+    conn.onmessage = function(event) {
+      appendLog("Message: " + event.data);
+    };
+
+    conn.onclose = function(event) {
+      appendLog("Connection closed");
+    };
+
+    conn.onerror = function(event) {
+      appendLog("Error: " + event.data);
+    };
+  });
+});
