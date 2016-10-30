cc5b89d72afbc20467d98b3517b192f53e4706d7
James Fargher <proglottis@gmail.com>
2016-10-30 13:04:59 +1300

    Let's encrypt

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"html/template"
 	"log"
 	"math/rand"
 	"net/http"
 	_ "net/http/pprof"
+	"net/url"
 	"os"
 	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/proglottis/tvgame/game"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 var (
@@ -28,12 +31,29 @@ func withLog(fn http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func websocketURL(r *http.Request) template.URL {
+	scheme := "ws"
+	if r.URL.Scheme == "https" {
+		scheme = "wss"
+	}
+	ws := &url.URL{
+		Scheme: scheme,
+		Host:   r.Host,
+		Path:   "ws",
+	}
+	return template.URL(ws.String())
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	flag.Parse()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
+	}
+	portTLS := os.Getenv("PORT_TLS")
+	if portTLS == "" {
+		portTLS = "8081"
 	}
 	csv, err := os.Open(flag.Arg(0))
 	if err != nil {
@@ -60,7 +80,7 @@ func main() {
 			return
 		}
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		index.Execute(w, r.Host)
+		index.Execute(w, websocketURL(r))
 	}))
 
 	http.HandleFunc("/host", withLog(func(w http.ResponseWriter, r *http.Request) {
@@ -73,7 +93,7 @@ func main() {
 			return
 		}
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		host.Execute(w, r.Host)
+		host.Execute(w, websocketURL(r))
 	}))
 
 	http.HandleFunc("/ws", withLog(func(w http.ResponseWriter, r *http.Request) {
@@ -87,6 +107,23 @@ func main() {
 			return
 		}
 	}))
+
+	go func() {
+		addr := ":" + portTLS
+		log.Printf("Starting TLS on %s", addr)
+		m := autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			Cache:      autocert.DirCache("cache"),
+			HostPolicy: autocert.HostWhitelist("tv.nothing.co.nz"),
+		}
+		s := &http.Server{
+			Addr:      addr,
+			TLSConfig: &tls.Config{GetCertificate: m.GetCertificate},
+		}
+		if err := s.ListenAndServeTLS("", ""); err != nil {
+			log.Fatal("ListenAndServeTLS:", err)
+		}
+	}()
 
 	addr := ":" + port
 	log.Printf("Starting on %s", addr)
