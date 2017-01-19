69f9e9bca2f92964f1135c4f2e0d82c446be9238
Cullum Deighton <cullumdeighton@Cullums-MacBook-Pro.local>
2017-01-19 14:35:45 +1300

    started changing views to phaser

--- js/host.js
+++ js/host.js
@@ -42,17 +42,17 @@ $(function() {
     };
   }
 
-  var conn = new WebSocket($('body').data('url'));
-
-  conn.onopen = function(event) {
-    console.log("Connection opened");
-    conn.send(JSON.stringify({type: "create"}));
-  };
-
-  var state = lobby;
-  conn.onmessage = function(event) {
-    state = state(event);
-  }
+  // var conn = new WebSocket($('body').data('url'));
+  // 
+  // conn.onopen = function(event) {
+  //   console.log("Connection opened");
+  //   conn.send(JSON.stringify({type: "create"}));
+  // };
+  // 
+  // var state = lobby;
+  // conn.onmessage = function(event) {
+  //   state = state(event);
+  // }
 
   function voteCollection(event) {
     var res   = JSON.parse(event.data),
@@ -165,21 +165,251 @@ $(function() {
     event.preventDefault();
     conn.send(JSON.stringify({type: "begin"}));
   });
-  
-  $().ready(function(){
-  });
 });
-
-
-const game = new Phaser.Game(800,600, Phaser.AUTO, '');
-game.state.add("start", new StartScene());
-game.state.start("start");
 
 function StartScene(){
   Phaser.State.call(this);
 }
 StartScene.prototype = Object.create(Phaser.State.prototype);
 
+StartScene.prototype.init = function(conn) {
+  this.conn = conn;
+}
+
 StartScene.prototype.create = function(){
-  this.start_button = new Phaser.Text(this.game, 50,50, "Everybody IN!");
+  console.log("StartScene");
 };
+
+StartScene.prototype.onConnection = function() {
+  this.conn.send(JSON.stringify({type: "create"}));
+}
+
+StartScene.prototype.onMessage = function(event) {
+  switch(event.Type) {
+    case "create":
+      this.state.start("lobby", true, false, this.conn, event.Data.Code);
+      break;
+    default:
+      console.log(event);
+  }
+}
+
+function LobbyScene(){
+  Phaser.State.call(this);
+}
+LobbyScene.prototype = Object.create(Phaser.State.prototype);
+
+LobbyScene.prototype.init = function(conn, code) {
+  this.conn = conn;
+  this.code = code;
+  this.players = [];
+};
+
+LobbyScene.prototype.preload = function() {
+  this.load.image("bg", "css/green-background.jpg");
+}
+
+LobbyScene.prototype.create = function() {
+  console.log("LobbyScene");
+
+  const bg = this.add.image(0, 0, "bg");
+  bg.width = this.world.width;
+  bg.height = this.world.height;
+  this.add.text(50,0, this.code, {fill: "#ff0000"});
+  this.start_button = this.add.text(50,50, "Everybody IN!", {fill: "#ff0000"});
+  this.start_button.inputEnabled = true;
+  this.start_button.events.onInputDown.add(this.listener, this);
+};
+
+LobbyScene.prototype.listener = function(){
+  this.conn.send(JSON.stringify({type: "begin"}));
+};
+
+LobbyScene.prototype.onMessage = function(event) {
+  switch(event.Type) {
+    case "question":
+      this.state.start("lie",true,false,this.conn, event.Data.Question);
+      break;
+    case "joined":
+      const player = event.Data.Player;
+      this.add.text(50,100+(this.players.length*50), player.Name, {fill: "#ff0000"});
+      this.players.push(player);
+      break;
+    default:
+      console.log(event);
+  }
+};
+
+function LieScene() {
+  Phaser.State.call(this);
+}
+
+LieScene.prototype = Object.create(Phaser.State.prototype);
+
+LieScene.prototype.preload = function() {
+  this.load.image("bg", "css/green-background.jpg");
+}
+
+LieScene.prototype.init = function(conn, question) {
+  this.conn = conn;
+  this.players = [];
+  this.question = question;
+};
+
+LieScene.prototype.create = function() {
+  console.log("LieScene");
+
+  const bg = this.add.image(0, 0, "bg");
+  bg.width = this.world.width;
+  bg.height = this.world.height;
+  const question = this.add.text(50,0, this.question.Text, {fill: "#ff0000", wordWrap: true, wordWrapWidth: this.world.width - 100});
+
+  this.timer = game.time.create(true);
+  this.timer.add(30000, this.endRound, this);
+  this.timer.start();
+};
+
+LieScene.prototype.endRound = function(){
+  conn.send(JSON.stringify({type: "stop"}));
+}
+
+LieScene.prototype.onMessage = function(event) {
+  switch(event.Type) {
+    case "collected":
+      if(event.Data.Complete) {
+        this.endRound();
+      }
+      this.add.text(50, 100+this.players.length*50, event.Data.Player.Name, {fill: "#ff0000"});
+      break;
+    case "vote":
+      this.state.start("vote", true, false, this.conn, event.Data.Question);
+      break;
+    default:
+      console.log(event);
+  }
+};
+
+function VoteScene() {
+  Phaser.State.call(this);
+}
+
+VoteScene.prototype = Object.create(Phaser.State.prototype);
+
+VoteScene.prototype.preload = function() {
+  this.load.image("bg", "css/green-background.jpg");
+}
+
+VoteScene.prototype.init = function(conn, question) {
+  this.conn = conn;
+  this.question = question;
+};
+
+VoteScene.prototype.create = function() {
+  console.log("VoteScene");
+
+  const bg = this.add.image(0, 0, "bg");
+  bg.width = this.world.width;
+  bg.height = this.world.height;
+  const question = this.add.text(50,0, this.question.Text, {fill: "#ff0000", wordWrap: true, wordWrapWidth: this.world.width - 100});
+
+  for(var i = 0; i < this.question.Answers.length; i++) {
+    const answer = this.question.Answers[i];
+    this.add.text(50, i*50+100, answer.Text, {fill: "#ff0000"});
+  }
+
+  this.timer = game.time.create(true);
+  this.timer.add(30000, this.endRound, this);
+  this.timer.start();
+};
+
+VoteScene.prototype.endRound = function(){
+  conn.send(JSON.stringify({type: "stop"}));
+}
+
+VoteScene.prototype.onMessage = function(event) {
+  switch(event.Type) {
+    case "collected":
+      if(event.Data.Complete) {
+        this.endRound();
+      }
+      break;
+    case "results":
+      this.state.start("score", true, false, this.conn, this.question, event.Data.Offsets, event.Data.Points);
+      break;
+    default:
+      console.log(event);
+  }
+};
+
+function ScoreScene() {
+  Phaser.State.call(this);
+}
+
+ScoreScene.prototype = Object.create(Phaser.State.prototype);
+
+ScoreScene.prototype.preload = function() {
+  this.load.image("bg", "css/green-background.jpg");
+}
+
+ScoreScene.prototype.init = function(conn, question, offsets, points) {
+  this.conn = conn;
+  this.question = question;
+  this.offsets = offsets;
+  this.points = points;
+};
+
+ScoreScene.prototype.create = function() {
+  console.log("ScoreScene");
+
+  const bg = this.add.image(0, 0, "bg");
+  bg.width = this.world.width;
+  bg.height = this.world.height;
+  const question = this.add.text(50,0, this.question.Text, {fill: "#ff0000", wordWrap: true, wordWrapWidth: this.world.width - 100});
+
+  const goodStyle = {fill: "#0000ff"};
+  const badStyle = {fill: "#ff0000"};
+
+  for(var i = 0; i < this.offsets.length; i++) {
+    const answer = this.offsets[i].Answer;
+    if (answer.Correct) {
+      this.add.text(50, i*50+100, `${answer.Text} - ${answer.Votes.length} votes`, goodStyle);
+    } else {
+      this.add.text(50, i*50+100, `${answer.Text} - ${answer.Votes.length} votes`, badStyle);
+    }
+  }
+
+  this.timer = game.time.create(true);
+  this.timer.add(30000, this.endRound, this);
+  this.timer.start();
+};
+
+ScoreScene.prototype.endRound = function(){
+}
+
+ScoreScene.prototype.onMessage = function(event) {
+  switch(event.Type) {
+    default:
+      console.log(event);
+  }
+};
+
+const game = new Phaser.Game(800,600, Phaser.AUTO, '');
+game.state.add("start", new StartScene());
+game.state.add("lobby", new LobbyScene());
+game.state.add("lie", new LieScene());
+game.state.add("vote", new VoteScene());
+game.state.add("score", new ScoreScene());
+
+const conn = new WebSocket($('body').data('url'));
+conn.onopen = function(event) {
+  const state = game.state.getCurrentState();
+  state.onConnection.call(state);
+};
+conn.onmessage = function(event) {
+  const state = game.state.getCurrentState();
+  const msg = JSON.parse(event.data);
+  console.log(msg.Type);
+  state.onMessage.call(state, msg);
+};
+
+game.state.start("start", true, false, conn);
